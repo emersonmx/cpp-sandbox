9bca17d1ebf1991f2808635e45b8f0b76838a02a
Emerson MX <emersonmx@gmail.com>
2018-02-11 13:59:57 -0300

    Add paranaues :D

---
+++ paranaues/const_extends.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+    const int NUMBER = 0;
+
+    virtual void print() {
+        std::cout << "A NUMBER: " << NUMBER << std::endl;
+    }
+};
+
+class B: public A {
+public:
+    const int NUMBER = 1;
+
+    void print() override {
+        std::cout << "B NUMBER: " << NUMBER << std::endl;
+    }
+};
+
+int main() {
+    A a;
+    B b;
+    A& c = b;
+    a.print();
+    b.print();
+    c.print();
+    return 0;
+}
