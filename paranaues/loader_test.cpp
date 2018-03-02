b812617fe5e1ecf35c2cf59e78fcd8481940da0b
Emerson MX <emersonmx@gmail.com>
2018-03-02 09:31:31 -0300

    Add loader example

---
+++ paranaues/loader_test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+
+class Texture {
+public:
+    Texture() {
+        std::cout << "Texture()" << std::endl;
+    }
+
+    virtual ~Texture() {
+        std::cout << "~Texture()" << std::endl;
+    }
+
+    void draw() {
+        std::cout << "Draw texture!" << std::endl;
+    }
+};
+
+template<typename T>
+class Loader {
+public:
+    virtual std::unique_ptr<T> load(const std::string& filepath) = 0;
+};
+
+class TextureLoader : public Loader<Texture> {
+public:
+    virtual std::unique_ptr<Texture> load(const std::string& filepath) {
+        return std::make_unique<Texture>();
+    }
+};
+
+int main() {
+    TextureLoader loader;
+    auto texture = loader.load("example.ext");
+    texture->draw();
+    return 0;
+}
