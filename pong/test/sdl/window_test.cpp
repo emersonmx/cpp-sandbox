e16cd2fbe6fe6d6f3222ae8dbb15bc554eda241a
Emerson MX <emersonmx@gmail.com>
2018-04-04 20:51:36 -0300

    Reorder test sections

--- pong/test/sdl/window_test.cpp
+++ pong/test/sdl/window_test.cpp
@@ -4,15 +4,21 @@
 
 #include <mxg/sdl/Window.hpp>
 
-TEST_CASE("Windows can be used", "[window]") {
+TEST_CASE("Windows can be created and closed", "[window]") {
     REQUIRE(SDL_Init(SDL_INIT_VIDEO) == 0);
 
     mxg::sdl::Window window;
 
     SECTION("creating without call create results in undefined behavior") {
-        REQUIRE_FALSE(window.isOpen());
+        REQUIRE(static_cast<SDL_Window*>(window) == nullptr);
         window.create("Test Window", {640, 480});
-        REQUIRE(window.isOpen());
+        REQUIRE(static_cast<SDL_Window*>(window) != nullptr);
+    }
+    SECTION("closing invalidates window") {
+        window.create("Test Window", {640, 480});
+        REQUIRE(static_cast<SDL_Window*>(window) != nullptr);
+        window.close();
+        REQUIRE(static_cast<SDL_Window*>(window) == nullptr);
     }
     SECTION("open is false when instantiated") {
         REQUIRE(window.isOpen() == false);
@@ -20,6 +26,10 @@ TEST_CASE("Windows can be used", "[window]") {
     SECTION("creating changes open to true") {
         window.create("Test Window", {640, 480});
         REQUIRE(window.isOpen() == true);
+    }
+    SECTION("closing changes open to false") {
+        window.close();
+        REQUIRE(window.isOpen() == false);
     }
     SECTION("creating with title changes it value") {
         window.create("Test Window", {640, 480});
@@ -30,16 +40,6 @@ TEST_CASE("Windows can be used", "[window]") {
         auto size = window.getSize();
         REQUIRE(size.width >= 640);
         REQUIRE(size.height >= 480);
-    }
-    SECTION("closing changes open to false") {
-        window.close();
-        REQUIRE(window.isOpen() == false);
-    }
-    SECTION("closing invalidates window") {
-        window.create("Test Window", {640, 480});
-        REQUIRE(window.isOpen());
-        window.close();
-        REQUIRE_FALSE(window.isOpen());
     }
 
     SDL_Quit();
